dd77ca157c66de5cd0d0f3ce2c6b22fe29981ae4
Berkay <info@berkaysenkazan.com>
2024-06-30 19:01:46 +0300

    Test?

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,6 +1,7 @@
 import { defineAuth } from '@aws-amplify/backend';
 import { UserAttributeKey } from 'aws-amplify/auth';
 
+
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
@@ -9,5 +10,16 @@ export const auth = defineAuth({
   loginWith: {
     email: true,
   },
+
+  userAttributes: {
+    birthdate: {
+      mutable: true,
+      required: false,
+    },
+    name: {
+      mutable: true,
+      required: false,
+    }
+  }
 
 });
--- src/Navigate.tsx
+++ src/Navigate.tsx
@@ -1,24 +1,60 @@
 
-import { Button, Flex, withAuthenticator } from '@aws-amplify/ui-react';
+import { Button, Flex, View, withAuthenticator } from '@aws-amplify/ui-react';
 import "./Navigate.css"
 import { Link } from "react-router-dom"
+import { fetchUserAttributes } from 'aws-amplify/auth';
+import { useEffect, useState } from 'react';
 
-function Navigate() {
+
+
+
+
+export function Navigate() {
+
+    const [userAttributes, setUserAttributes] = useState("");
+
+    useEffect(() => {
+        const GetAttributes = async () => {
+            try {
+                const userAttr = await fetchUserAttributes();
+                setUserAttributes(userAttr.email!.toUpperCase)
+            } catch (error)
+            {
+                setUserAttributes("nope" + error)
+            }
+      
+
+        }
+        GetAttributes();
+        
+
+    })
+
+
+
+    
 
     return(
 
-    <div id="NavBar">
-        <Flex className="FlexNav" direction={"column"}>
-            
+    <div id="NavBar" className='NavBar'>
+        <View className='View' as='div'>
 
-            <Link to={"/doplay"}> <Button> but </Button></Link>
+        <Flex className="FlexNav" direction={"row"}>
+            
+            
+            <h1>{userAttributes}</h1>
+
+            <Link to={"/doplay"} > <Button className='Button'> DoPlay </Button></Link>
 
  
 
-            <Button className="Button">
-                DoPlay
-            </Button>
-        </Flex>      
+            <Link to={"/doplay"}><Button className='Button'>DoPlay</Button></Link>
+
+        </Flex>
+
+        </View>
+
+              
     </div>
        
     )
