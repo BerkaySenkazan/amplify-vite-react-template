71734fff30faed90b5dfad7e8b8ec6dfcf5850f1
Berkay <info@berkaysenkazan.com>
2024-07-03 19:40:25 +0300

    auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -6,24 +6,4 @@ import { UserAttributeKey } from 'aws-amplify/auth';
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
  */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
 
-  userAttributes: {
-    birthdate: {
-      mutable: true,
-      required: false,
-    },
-    givenName: {
-      mutable: true,
-      required: false,
-    },
-    email: {
-      mutable: true,
-      required: true,
-    }
-  }
-
-});
