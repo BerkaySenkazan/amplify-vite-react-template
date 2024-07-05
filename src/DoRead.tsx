3f0c3d1e4b203f48e2a33f5c1c4fe26993736663
Berkay <info@berkaysenkazan.com>
2024-07-05 19:48:59 +0300

    dolearn

--- src/DoRead.tsx
+++ src/DoRead.tsx
@@ -8,7 +8,7 @@ function DoRead(){
         <Flex position={'fixed'} width={"100%"} height={"100%"} direction={"column"} gap={"0"} backgroundColor={"black"}>
             <iframe src='TestGame.html' height={"100%"} width={"100%"} />
             <Text textAlign={'center'} padding={"0"} color={'white'}>Berkay Şenkazan</Text>
-            <Text textAlign={'center'} padding={"0"} color={'white'}>2023</Text>
+            <Text textAlign={'center'} padding={"0"} color={'white'}>2023 - 05 - 07</Text>
         </Flex>
         
         
