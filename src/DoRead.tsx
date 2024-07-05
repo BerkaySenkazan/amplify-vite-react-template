e6e5e3827aacb7320dd6084905fd58a2601dae23
Berkay <info@berkaysenkazan.com>
2024-07-05 22:05:15 +0300

    srtc

--- src/DoRead.tsx
+++ src/DoRead.tsx
@@ -2,11 +2,11 @@ import { Flex, withAuthenticator, Text } from '@aws-amplify/ui-react';
 
 function DoRead(){
 
-   
+
 
     return(
         <Flex position={'fixed'} width={"100%"} height={"100%"} direction={"column"} gap={"0"} backgroundColor={"black"}>
-            <iframe srcDoc='TestGame.html' height={"100%"} width={"100%"} />
+            <iframe src='public/TestGame.html' height={"100%"} width={"100%"} />
             <Text textAlign={'center'} padding={"0"} color={'white'}>Berkay Şenkazan</Text>
             <Text textAlign={'center'} padding={"0"} color={'white'}>2023 - 05 - 07</Text>
         </Flex>
