9a22a8dcd058d720ded71728ed712ae20184cbdc
Berkay <info@berkaysenkazan.com>
2024-07-05 21:55:01 +0300

    pathways

--- src/DoRead.tsx
+++ src/DoRead.tsx
@@ -6,7 +6,7 @@ function DoRead(){
 
     return(
         <Flex position={'fixed'} width={"100%"} height={"100%"} direction={"column"} gap={"0"} backgroundColor={"black"}>
-            <iframe src='TestGame.html' height={"100%"} width={"100%"} />
+            <iframe src='./public/TestGame.html' height={"100%"} width={"100%"} />
             <Text textAlign={'center'} padding={"0"} color={'white'}>Berkay Şenkazan</Text>
             <Text textAlign={'center'} padding={"0"} color={'white'}>2023 - 05 - 07</Text>
         </Flex>
